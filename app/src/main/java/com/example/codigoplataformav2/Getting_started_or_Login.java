27a78d0aecc767676ae22c58ac0ddc31a46c30ec
Mahaveer86619 <psatyam86619@gmail.com>
2023-02-16 16:55:12 +0530

    SignUp page and Startup pages made

---
+++ app/src/main/java/com/example/codigoplataformav2/Getting_started_or_Login.java
@@ -0,0 +1,14 @@
+package com.example.codigoplataformav2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Getting_started_or_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getting_started_or_login);
+    }
+}
