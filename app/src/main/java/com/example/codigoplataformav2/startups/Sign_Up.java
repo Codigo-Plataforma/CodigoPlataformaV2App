6d8d92d902beb61f5dfd76abef4f8373834ab4c7
Mahaveer86619 <psatyam86619@gmail.com>
2023-02-22 22:41:30 +0530

    few tweaks 2

--- app/src/main/java/com/example/codigoplataformav2/startups/Sign_Up.java
+++ app/src/main/java/com/example/codigoplataformav2/startups/Sign_Up.java
@@ -2,20 +2,20 @@ package com.example.codigoplataformav2.startups;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.codigoplataformav2.R;
 
-import com.example.codigoplataformav2.R;
 
 public class Sign_Up extends AppCompatActivity {
-    Button back;
+    ImageButton back;
     TextView login;
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,30 +28,24 @@ public class Sign_Up extends AppCompatActivity {
 
         setContentView(R.layout.activity_sign_up);
 
-        back = findViewById(R.id.btn_backbtn);
+        back = findViewById(R.id.backButton);
 
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Sign_Up.this, Getting_started_or_Login.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                startActivity(i);
+        back.setOnClickListener(v -> {
+            Intent i = new Intent(Sign_Up.this, Getting_started_or_Login.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(i);
 
-                finish();
-            }
+            finish();
         });
 
         login = findViewById(R.id.btn_Login_in_sign_up);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Sign_Up.this,LoginPage.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                startActivity(i);
+        login.setOnClickListener(v -> {
+            Intent i = new Intent(Sign_Up.this,LoginPage.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(i);
 
-                finish();
-            }
+            finish();
         });
     }
 }
